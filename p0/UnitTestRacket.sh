93255e52b886c6a555609667f762b8e1744eab4a
Kobi Luria <kobi@luria.net>
2014-11-04 20:46:08 -0500

    Changed the travis file to run a script on p0 of sicp

---
+++ p0/UnitTestRacket.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+####################################
+# Add a prefix out for each Module #
+####################################
+for f in *.rkt
+do
+    name="${f%.*}"
+    echo "\n    (provide(prefix-out "$name": (all-defined-out)))" >> $f
+done
+
+##########################################
+# Create Test Module and add a few tests #
+##########################################
+
+echo '#lang racket' >> test.txt
+echo '(require rackunit)' >> test.txt
+
+for f in *.rkt
+do
+    echo "(require \"$f\")" >> test.txt
+done
+##########################################
+# Create Test Module and add a few tests #
+##########################################
+echo  '(module+ test' >> test.txt
+echo -n '(check-equal?' >> test.txt
+
+for f in *.rkt
+do
+    name="${f%.*}" 
+    echo -n " ($name:bitfunc 2)" >> test.txt
+done
+echo -n " 0" >> test.txt
+echo -n " \"Simple bitFunc\")" >> test.txt
+echo -n ')' >> test.txt
+mv test.txt test.rkt
+
